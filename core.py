5852a34a82afdb24b7b7f83e7b90404fd55499fa
Odin Hoff Gardaa <odinhg@gmail.com>
2024-04-18 09:45:43 +0200

    merged alpha core into a single function in core.py and added notebooks (not done)

--- core.py
+++ core.py
@@ -3,13 +3,27 @@ import numpy as np
 from miniball import Miniball
 from scipy.spatial import KDTree
 from itertools import combinations
+from math import sqrt
 
 
-def core_cech(points, k, max_dim=1, max_r=None, return_squared=False):
+def core_cech(
+    points: np.ndarray,
+    k: int,
+    max_dim: int = 1,
+    max_r: float | None = None,
+    return_squared: bool = False,
+) -> gudhi.SimplexTree:
     """
-    Compute the core Čech filtration for a fixed k (if max_r is None) or along the line passing through (0, k) and (max_r, 0).
+    Compute the core Čech filtration for a fixed k if max_r is None. Otherwise, compute the filtration along the line passing through (0, k) and (max_r, 0).
 
-    TODO: Write docstring.
+    Args:
+        points: Input point cloud of shape (n, d).
+        k: Value of k as described above.
+        max_dim: Add simplices of dimensions up to and including max_dim + 1.
+        max_r: Value of max_r as described above.
+        return_squared: Return squared filtration values if True, default value is False.
+    Returns:
+        The core Čech filtration as a simplex tree.
     """
 
     kd_tree = KDTree(points)
@@ -24,51 +38,56 @@ def core_cech(points, k, max_dim=1, max_r=None, return_squared=False):
         line = np.linspace(max_r, 0, num=k)
         vertex_indices = np.argmax(line <= k_core_distances, axis=1)
         vertex_values = k_core_distances[np.arange(len(points)), vertex_indices]
-    
+
     st = gudhi.SimplexTree()
     for dim in range(max_dim + 1):
         for face in combinations(range(len(points)), dim + 1):
             face_list = list(face)
             max_core = max(vertex_values[face_list])
             squared_radius = Miniball(points[face_list]).squared_radius()
-            if not return_squared:
-                st.insert(face, max(squared_radius**0.5, max_core))
+            if return_squared:
+                filtration_value = max(squared_radius, max_core**2)
             else:
-                st.insert(face, max(squared_radius, max_core**2))
+                filtration_value = max(sqrt(squared_radius), max_core)
+            st.insert(face, filtration_value)
     return st
 
 
-def alpha_core(points, k=1):
+def alpha_core(
+    points: np.ndarray, k: int, max_r: float | None = None, return_squared: bool = False
+) -> gudhi.SimplexTree:
+    """
+    Compute the alpha-core filtration for a fixed k if max_r is None. Otherwise, compute the filtration along the line passing through (0, k) and (max_r, 0).
+
+    Args:
+        points: Input point cloud of shape (n, d).
+        k: Value of k as described above.
+        max_r: Value of max_r as described above.
+        return_squared: Return squared filtration values if True, default value is False.
+    Returns:
+        The alpha-core filtration as a simplex tree.
+    """
+
     kd_tree = KDTree(points)
     k_core_distances, _ = kd_tree.query(points, k=k)
-    if k > 1:
-        k_core_distances = k_core_distances[:, -1]
-    st_alpha = gudhi.AlphaComplex(points=points).create_simplex_tree()
-    for face, val in st_alpha.get_filtration():
-        max_core = max(k_core_distances[list(face)])
-        st_alpha.assign_filtration(face, max(val**0.5, max_core))
-    return st_alpha
 
+    if k == 1:
+        k_core_distances = k_core_distances[..., np.newaxis]
 
-def alpha_core_slope(points, max_k=None, max_r=None):
-    if max_k is None:
-        max_k = len(points)
     if max_r is None:
-        max_r = 2 * np.sqrt(Miniball(points).squared_radius())
-    kd_tree = KDTree(points)
-    k_core_distances, _ = kd_tree.query(points, k=max_k)
-    line = np.linspace(max_r, 0, num=max_k)
-    if max_k > 1:
-        vertex_indices = np.argmax(line <= k_core_distances, axis=1)
-        vertex_values = k_core_distances[
-            np.arange(len(k_core_distances)), vertex_indices
-        ]
+        vertex_values = k_core_distances[:, -1]
     else:
-        vertex_values = np.array(k_core_distances)
-    st_alpha = gudhi.AlphaComplex(points=points).create_simplex_tree()
-    for face, val in st_alpha.get_filtration():
+        line = np.linspace(max_r, 0, num=k)
+        vertex_indices = np.argmax(line <= k_core_distances, axis=1)
+        vertex_values = k_core_distances[np.arange(len(points)), vertex_indices]
+
+    st = gudhi.AlphaComplex(points=points).create_simplex_tree()
+    for face, val in st.get_filtration():
         face_list = list(face)
         max_core = max(vertex_values[face_list])
-        new_val = min(max(val**0.5, max_core), max_r)
-        st_alpha.assign_filtration(face, new_val)
-    return st_alpha
+        if return_squared:
+            new_val = max(val, max_core**2)
+        else:
+            new_val = max(sqrt(val), max_core)
+        st.assign_filtration(face, new_val)
+    return st
