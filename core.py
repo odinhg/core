3243633e4f37ae1c5beffb22b3e465d0e5ac559c
lar-sal <lars.salbu@uib.no>
2024-04-24 21:01:30 +0200

    More datasets/experiments + Mortens (new) code implemented

--- core.py
+++ core.py
@@ -6,92 +6,81 @@ from itertools import combinations
 from math import sqrt
 from typing import Union
 
+# Compute distances to k-nearest neighbors in X
+def k_nearest_neighbor_distances(X, k):
+    kd_tree = KDTree(X)
+    k_core_distances, _ = kd_tree.query(X, k=k, workers=-1)
+    return k_core_distances  
 
-def core_cech(
-    points: np.ndarray,
-    k: int,
-    max_dim: int = 1,
-    max_r: Union[float, None] = None,
-    return_squared: bool = False,
-) -> gudhi.SimplexTree:
-    """
-    Compute the core Čech filtration for a fixed k if max_r is None. Otherwise, compute the filtration along the line passing through (0, k) and (max_r, 0).
+# Compute filtration values of faces from squared radius and core values
+def core_value(face, squared_radius, core_values):
+    max_core = max(core_values[face]) 
+    return max(squared_radius ** 0.5, max_core)
 
-    Args:
-        points: Input point cloud of shape (n, d).
-        k: Value of k as described above.
-        max_dim: Add simplices of dimensions up to and including max_dim + 1.
-        max_r: Value of max_r as described above.
-        return_squared: Return squared filtration values if True, default value is False.
-    Returns:
-        The core Čech filtration as a simplex tree.
-    """
 
-    kd_tree = KDTree(points)
-    k_core_distances, _ = kd_tree.query(points, k=k)
-
-    if k == 1:
-        k_core_distances = k_core_distances[..., np.newaxis]
-
-    if max_r is None:
-        vertex_values = k_core_distances[:, -1]
+# Compute core value for each points
+def vertex_values(X, max_k, max_r):
+    if max_r is None or max_k <= 1:
+        return k_nearest_neighbor_distances(X, k=[max_k])
+    k_core_distances = k_nearest_neighbor_distances(X, np.arange(1, max_k + 1))
+    line = np.linspace(max_r, 0, num=max_k)
+    if max_k>1:
+        indices = np.argmax(line <= k_core_distances, axis=1)
+        values = k_core_distances[np.arange(len(k_core_distances)), indices]
+        values[values > max_r] = max_r
     else:
-        line = np.linspace(max_r, 0, num=k)
-        vertex_indices = np.argmax(line <= k_core_distances, axis=1)
-        vertex_values = k_core_distances[np.arange(len(points)), vertex_indices]
+        values = np.array(k_core_distances)
+    return values
 
+# Compute core simplex tree from a simplex tree filtered by squared radius and a point cloud
+def core_complex(X, st, max_k, max_r=None):
+    k_core_distances = vertex_values(X, max_k, max_r)**2
+    for vertex in range(X.shape[0]):
+        st.assign_filtration([vertex], k_core_distances[vertex])
+    st.make_filtration_non_decreasing()
+    return st
+
+# Compute simplex tree of a point cloud filtered by squared radius
+def cech_squared_radius(X, max_dim=1):
     st = gudhi.SimplexTree()
-    for dim in range(max_dim + 1):
-        for face in combinations(range(len(points)), dim + 1):
-            face_list = list(face)
-            max_core = max(vertex_values[face_list])
-            squared_radius = Miniball(points[face_list]).squared_radius()
-            if return_squared:
-                filtration_value = max(squared_radius, max_core**2)
-            else:
-                filtration_value = max(sqrt(squared_radius), max_core)
-            st.insert(face, filtration_value)
+    for dim in range(max_dim+1):
+        for face in combinations(range(len(X)),dim+1):
+            val = Miniball(X[list(face)]).squared_radius()
+            st.insert(face, val)
     return st
 
 
-def alpha_core(
-    points: np.ndarray,
-    k: int,
-    max_r: Union[float, None] = None,
-    return_squared: bool = False,
-) -> gudhi.SimplexTree:
+def core_cech(X, max_k=10, max_r=None, max_dim=1):
+    """
+    Compute the core Čech filtration for a fixed k if max_r is None. Otherwise, compute the filtration along the line passing through (0, k) and (max_r, 0).
+
+    Inputs:
+        X: Input point cloud of shape (n, d).
+        k: Value of k as described above.
+        max_dim: Add simplices of dimensions up to and including max_dim + 1.
+        max_r: Value of max_r as described above.
+    Returns:
+        The core Čech filtration as a simplex tree.
+    """
+    st = cech_squared_radius(X, max_dim=max_dim)
+    return core_complex(X, st, max_k, max_r)
+
+def alpha_core(X, max_k=10, max_r=None, precision='safe'):
     """
     Compute the alpha-core filtration for a fixed k if max_r is None. Otherwise, compute the filtration along the line passing through (0, k) and (max_r, 0).
 
     Args:
-        points: Input point cloud of shape (n, d).
+        X: Input point cloud of shape (n, d).
         k: Value of k as described above.
         max_r: Value of max_r as described above.
-        return_squared: Return squared filtration values if True, default value is False.
+        precision: Precision for Gudhis Alpha complex, can be ‘fast’, ‘safe’ or ‘exact’.
     Returns:
         The alpha-core filtration as a simplex tree.
     """
+    st = gudhi.AlphaComplex(points=X, precision=precision).create_simplex_tree()
+    return core_complex(X, st, max_k, max_r)
 
-    kd_tree = KDTree(points)
-    k_core_distances, _ = kd_tree.query(points, k=k)
-
-    if k == 1:
-        k_core_distances = k_core_distances[..., np.newaxis]
-
-    if max_r is None:
-        vertex_values = k_core_distances[:, -1]
-    else:
-        line = np.linspace(max_r, 0, num=k)
-        vertex_indices = np.argmax(line <= k_core_distances, axis=1)
-        vertex_values = k_core_distances[np.arange(len(points)), vertex_indices]
-
-    st = gudhi.AlphaComplex(points=points).create_simplex_tree()
-    for face, val in st.get_filtration():
-        face_list = list(face)
-        max_core = max(vertex_values[face_list])
-        if return_squared:
-            new_val = max(val, max_core**2)
-        else:
-            new_val = max(sqrt(val), max_core)
-        st.assign_filtration(face, new_val)
-    return st
+def plot_sqrt_persistence(st, axes):
+    persistence = st.persistence()
+    persistence = [(dim, (birth**.5, death**.5)) for dim, (birth, death) in persistence]
+    return gudhi.plot_persistence_diagram(persistence, axes=axes)
--- datasets.py
+++ datasets.py
@@ -1,12 +1,10 @@
 import numpy as np
 from typing import Tuple
 
-
 def sample_circle(
     n: int,
     generator: np.random.Generator,
     r: float = 1.0,
-    center: Tuple[float, float] = (0.0, 0.0),
     variance: float = 0.0,
 ) -> np.ndarray:
     t = generator.uniform(0, 2 * np.pi, n)
@@ -15,18 +13,20 @@ def sample_circle(
         X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 2))
     return X
 
-
-def sample_rectangle(
+def sample_unit_sphere(
     n: int,
     generator: np.random.Generator,
-    lower_left_corner: Tuple[float, float] = (-1, -1),
-    upper_right_corner: Tuple[float, float] = (1, 1),
+    variance: float = 0.0,
 ) -> np.ndarray:
-    x1, y1 = lower_left_corner
-    x2, y2 = upper_right_corner
-    X = np.c_[generator.uniform(x1, x2, n), generator.uniform(y1, y2, n)]
-    return X
+    Z1=generator.normal(size=n)
+    Z2=generator.normal(size=n)
+    Z3=generator.normal(size=n)
+    r=np.sqrt(Z1**2+Z2**2+Z3**2)
+    X = np.c_[Z1/r, Z2/r, Z3/r]
 
+    if variance > 0.0:
+        X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 3))
+    return X
 
 def sample_torus(
     n: int,
@@ -44,6 +44,53 @@ def sample_torus(
         X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 3))
     return X
 
+def sample_double_circle(
+    n: int,
+    generator: np.random.Generator,
+    r: float = 1.0,
+    variance: float = 0.0,
+) -> np.ndarray:
+    n1 = int(2*n/3)
+    n2 = n-n1
+    t1 = generator.uniform(0, 2 * np.pi, n1)
+    t2 = generator.uniform(0, 2 * np.pi, n2)
+    X1 = r * np.c_[np.cos(t1), np.sin(t1)]
+    X2 = (r/2) * np.c_[np.cos(t2), np.sin(t2)]
+    X = np.concatenate((X1, X2), axis=0)
+    if variance > 0.0:
+        X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 2))
+    return X
+
+# Clifford (flat) torus embedded in the unit 3-sphere in R^4
+def sample_clifford_torus(
+    n: int,
+    generator: np.random.Generator,
+    variance: float = 0.0,
+) -> np.ndarray:
+    ...
+    t1 = generator.uniform(0, 2 * np.pi, n)
+    t2 = generator.uniform(0, 2 * np.pi, n)
+    X1 = (1/(2**0.5)) * np.c_[np.cos(t1), np.sin(t1)]
+    X2 = (1/(2**0.5)) * np.c_[np.cos(t2), np.sin(t2)]
+    
+    X = np.concatenate((X1, X2), axis=1)
+    if variance > 0.0:
+        X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 4))
+    return X
+
+
+
+def sample_rectangle(
+    n: int,
+    generator: np.random.Generator,
+    lower_left_corner: Tuple[float, float] = (-1, -1),
+    upper_right_corner: Tuple[float, float] = (1, 1),
+) -> np.ndarray:
+    x1, y1 = lower_left_corner
+    x2, y2 = upper_right_corner
+    X = np.c_[generator.uniform(x1, x2, n), generator.uniform(y1, y2, n)]
+    return X
+
 
 def sample_cube(
     n: int,
@@ -57,6 +104,22 @@ def sample_cube(
         generator.uniform(x1, x2, n),
         generator.uniform(y1, y2, n),
         generator.uniform(z1, z2, n),
+    ]
+    return X
+
+def sample_4_cube(
+    n: int,
+    generator: np.random.Generator,
+    lower_left_corner: Tuple[float, float, float, float] = (-1, -1, -1, -1),
+    upper_right_corner: Tuple[float, float, float, float] = (1, 1, 1, 1),
+) -> np.ndarray:
+    x1, y1, z1, w1 = lower_left_corner
+    x2, y2, z2, w2 = upper_right_corner
+    X = np.c_[
+        generator.uniform(x1, x2, n),
+        generator.uniform(y1, y2, n),
+        generator.uniform(z1, z2, n),
+        generator.uniform(w1, w2, n),
     ]
     return X
 
