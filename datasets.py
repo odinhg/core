5853f3c97765be6e04fd78d8f2ccfd0d207c8858
Odin Hoff Gardaa <odinhg@gmail.com>
2024-04-24 10:38:25 +0200

    added torus and cube datasets

--- datasets.py
+++ datasets.py
@@ -8,7 +8,7 @@ def sample_circle(
     center: tuple[float] = (0.0, 0.0),
     variance: float = 0.0,
 ) -> np.ndarray:
-    #t = generator.uniform(0, 2 * np.pi, n)
+    # t = generator.uniform(0, 2 * np.pi, n)
     t = np.linspace(0, 2 * np.pi, n, endpoint=False)
     X = r * np.c_[np.cos(t), np.sin(t)]
     if variance > 0.0:
@@ -28,15 +28,56 @@ def sample_rectangle(
     return X
 
 
+def sample_torus(
+    n: int,
+    generator: np.random.Generator,
+    a: float = 0.25,
+    b: float = 0.75,
+    variance: float = 0.0,
+) -> np.ndarray:
+    ...
+    t1 = generator.uniform(0, 2 * np.pi, n)
+    t2 = generator.uniform(0, 2 * np.pi, n)
+    r = a * np.cos(t1) + b
+    X = np.c_[r * np.cos(t2), r * np.sin(t2), a * np.sin(t1)]
+    if variance > 0.0:
+        X += generator.normal(loc=0.0, scale=np.sqrt(variance), size=(n, 3))
+    return X
+
+
+def sample_cube(
+    n: int,
+    generator: np.random.Generator,
+    lower_left_corner: tuple[float] = (-1, -1, -1),
+    upper_right_corner: tuple[float] = (1, 1, 1),
+) -> np.ndarray:
+    x1, y1, z1 = lower_left_corner
+    x2, y2, z2 = upper_right_corner
+    X = np.c_[
+        generator.uniform(x1, x2, n),
+        generator.uniform(y1, y2, n),
+        generator.uniform(z1, z2, n),
+    ]
+    return X
+
+
 if __name__ == "__main__":
-    """
-    Example usage:
-    """
     import matplotlib.pyplot as plt
 
     generator = np.random.default_rng(seed=0)
-    X = sample_circle(50, generator, variance=0.01)
-    Y = sample_rectangle(50, generator)
+
+    # Torus with noise
+    X = sample_torus(5000, generator, variance=0.01)
+    Y = sample_cube(1000, generator)
     Z = np.r_[X, Y]
-    plt.scatter(*Z.T)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter(*Z.T, alpha=0.2, color="black", s=5)
+    plt.show()
+
+    # Circle with noise
+    X = sample_circle(200, generator, variance=0.01)
+    Y = sample_rectangle(100, generator)
+    Z = np.r_[X, Y]
+    plt.scatter(*Z.T, color="black", s=5)
     plt.show()
