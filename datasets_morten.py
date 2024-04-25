89527b7eea13b0221ec6767b2d818d90f00ca50f
Odin Hoff Gardaa <odinhg@gmail.com>
2024-04-25 18:04:20 +0200

    sampling from torus and sphere is now uniform

--- datasets_morten.py
+++ datasets_morten.py
@@ -29,18 +29,18 @@ def sample_torus(
     b: float = 0.75,
     std: float = 0.0,
 ) -> np.ndarray:
-    ...
-    # C1 = sample_circle(n, generator, r=a, std=0)
-    # C2 = sample_circle(n, generator, r=1, std=0)
-    # r = a * C1[:, 0] + b
-    # X = np.c_[r * C2[:, 0], r * C2[:, 1], a * C1[:, 1]]
-    t1 = generator.uniform(0, 2 * np.pi, n)
-    t2 = generator.uniform(0, 2 * np.pi, n)
-    r = a * np.cos(t1) + b
-    X = np.c_[r * np.cos(t2), r * np.sin(t2), a * np.sin(t1)]
+    points = []
+    while len(points) < n:
+        u, v, w = generator.uniform(0, 1, 3)
+        t1, t2 = 2 * np.pi * u, 2 * np.pi * v
+        if w <= (b + a * np.cos(t1)) / (a + b):
+            r = a * np.cos(t1) + b
+            points.append([r * np.cos(t2), r * np.sin(t2), a * np.sin(t1)])
+    X = np.array(points) 
     if std > 0.0:
         X += generator.normal(loc=0.0, scale=std, size=(n, 3))
     return X
+
 
 
 def sample_cube(
@@ -68,7 +68,7 @@ def sample_sphere(
     generator: np.random.Generator,
     std: float = 0.1,
 ) -> np.ndarray:
-    X = generator.uniform(-1, 1, (n, 3))
+    X = generator.standard_normal((n, 3))
     X /= np.linalg.norm(X, axis=1)[:, np.newaxis]
     X = X + generator.normal(loc=0.0, scale=std, size=(n, 3))
     return X
